2ba3065093d086fc79ee343564f806cb29865190
kristian <kfranzen@kth.se>
2022-11-30 16:28:35 +0100

    Loading pysical memory

--- Lab3/main.c
+++ Lab3/main.c
@@ -12,9 +12,9 @@ int extract_offset(int addr){
 }
 
 int main(int argc, char *argv[]){
-
+    int pageSice = 256;
     int addr[7] = {1, 256, 32768, 32769, 128, 65534, 33153};
-
+    int physicalMemory[pageSice][pageSice];
     char* filename;
 
     if(argc > 1){
@@ -35,8 +35,33 @@ int main(int argc, char *argv[]){
     while (fgets(line, sizeof(line), file)) {
         page = extract_page(atoi(line));
         offset = extract_offset(atoi(line));
-        printf("Page: %d, Offset: %d \n", page, offset);
+        // printf("Page: %d, Offset: %d \n", page, offset);
     }
+
+    //Function start here
+    int addres = 53683;
+    
+    unsigned char buffer[pageSice];
+    int startOfPage = addres % pageSice;  //  53504
+    FILE *file2;
+    
+    file2 = fopen("data/BACKING_STORE.bin","rb");  // r for read, b for binary
+    
+    fseek(file2, addres, SEEK_SET);  //file är backstoreing, 3098 är ?, seek_set= ??
+
+    fread(buffer,sizeof(buffer),1,file2);
+
+                
+    int frameNumber = startOfPage / 256;            //find frame number
+    printf("The frame loaded is: \n");
+    for(int i = 0; i < pageSice; i++){  	        //read out the frame got.
+        physicalMemory[frameNumber][i] = buffer[i];     //fills in the pysical memory from what was gatherd
+        printf("%d ", physicalMemory[frameNumber][i]);   
+    } 
+        printf("\n");
+
+
+    //Function end here
 
     exit(0);
 }
--- Lab3/readBinary.c
+++ Lab3/readBinary.c
@@ -8,11 +8,13 @@ int main(){
 
     file = fopen("data/BACKING_STORE.bin","rb");  // r for read, b for binary
     
-    fseek(file, 30198, SEEK_SET);
+    fseek(file, 53504, SEEK_SET);  //file är backstoreing, 3098 är ?, seek_set= ??
 
     fread(buffer,sizeof(buffer),1,file);
 
-    //for(int i = 0; i<256; i++)
+// for(int i = 0; i<256; i++){  	        //read out the frame got.
+//     printf("%u ", buffer[i]); 
+// }
     printf("%u ", buffer[0]); 
 
     printf("\n");
