d75a705c754de0b204436aea36822238ef0034e0
kristian <kfranzen@kth.se>
2022-12-02 11:57:35 +0100

    Working page fault

--- Lab3/main.c
+++ Lab3/main.c
@@ -51,64 +51,53 @@ int main(int argc, char *argv[]){
     int offset;
     int addres;
     int switchVar =1;
-    unsigned char buffer[SIZE];
+    char buffer[SIZE];
     int startOfPage;
     int frameNumber;
-    int whatIsThisValue = 1010101;
     int pysicalAdress;
     
     while (fgets(line, sizeof(line), file)) {
         pageNumber = extract_page(atoi(line));
         offset = extract_offset(atoi(line));
-        // printf("Page: %d, Offset: %d \n", page, offset);
+       
         addres = atoi(line);
         
-    //Function start here
+    
 
         switch (switchVar)
         {
         case 1:
             
-            startOfPage = addres % SIZE;  //  53504
+            startOfPage = addres - offset; 
             FILE *file2;
             
-            file2 = fopen("data/BACKING_STORE.bin","rb");  // r for read, b for binary
+            file2 = fopen("data/BACKING_STORE.bin","rb"); 
             
-            fseek(file2, addres, SEEK_SET);  //file är backstoreing, 3098 är ?, seek_set= ??
+            fseek(file2, startOfPage, SEEK_SET);  
 
             fread(buffer,sizeof(buffer),1,file2);
 
-                        
-            //frameNumber = addres / 256;            //find frame number
-            //printf("The frame loaded is: \n");
-            //printf("Pysicla memory");
-
+                    
 
             frameNumber = find_free();
 
              
-            for(int i = 0; i < SIZE; i++){  	        //read out the frame got.
-                
-                if(addres == 62493){
-                    printf("%d ", buffer[i]);
-                }
-                physicalMemory[frameNumber][i] = buffer[i];     //fills in the pysical memory from what was gatherd
-             //   printf("%d ", physicalMemory[frameNumber][i]);   
+            for(int i = 0; i < SIZE; i++){  	       
+                physicalMemory[frameNumber][i] = buffer[i];        
             } 
             pysicalAdress = frameNumber * SIZE + offset;
           
-              if(addres == 62493){
-                    printf("%d this is offcet", offset);
-            }
-            
+   
+
             pageTabel[pageNumber] = frameNumber;
             printf("Virtual address: %d Physical address: %d Value: %d", addres, pysicalAdress, physicalMemory[frameNumber][offset]);
             printf("\n");
-            //Virtual address: 53683 Physical address: 947 Value: 108
-            //Function end here
+           
             break;
 
         case 2:
+
+        
             break;
         }
   
